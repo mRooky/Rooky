440be7044ce605c2c9250ec3ff28f9e9cf8e9afb
mRooky <rookyma@hotmail.com>
2019-03-14 15:06:13 +0800

    test

--- Example/ExampleTexture.cpp
+++ Example/ExampleTexture.cpp
@@ -6,8 +6,6 @@
  */
 
 #include "ExampleTexture.h"
-#include <FreeImage.h>
-
 #include <iostream>
 
 namespace Example
@@ -29,32 +27,7 @@ void Texture::Initialize(void)
 
 void Texture::CreateTexture(const char* file)
 {
-	FreeImage_Initialise(TRUE);
-	FREE_IMAGE_FORMAT fif = FreeImage_GetFileType(file);
-	if (fif == FIF_UNKNOWN)
-	{
-		fif = FreeImage_GetFIFFromFilename(file);
-	}
 
-	if (fif != FIF_UNKNOWN)
-	{
-		BOOL support = FreeImage_FIFSupportsReading(fif);
-		if (support)
-		{
-			FIBITMAP *bimap = FreeImage_Load(fif, file);
-			int Width = FreeImage_GetWidth(bimap);
-			int Height = FreeImage_GetHeight(bimap);
-			int ByteWidth = FreeImage_GetLine(bimap);
-			std::cout << "FreeImage BPP : " << FreeImage_GetBPP(bimap) << std::endl;
-			std::cout << "FreeImage ImageType : " << FreeImage_GetImageType(bimap) << std::endl;
-			std::cout << "FreeImage ColorsUsed : " << FreeImage_GetColorsUsed(bimap) << std::endl;
-			std::cout << "FreeImage Layout : " << Width << " " << Height << " " << ByteWidth << std::endl;
-			std::cout << "FreeImage DIBSize : " << FreeImage_GetDIBSize(bimap) << std::endl;
-			FreeImage_Unload(bimap);
-			bimap = nullptr;
-		}
-	}
-	FreeImage_DeInitialise();
 }
 
 } /* namespace Example */
