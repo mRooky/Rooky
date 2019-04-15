4e2e5796692c1a219210a4b5655d5c77f3078cd2
mRooky <rookyma@hotmail.com>
2019-04-15 20:37:12 +0800

    memory op

---
+++ Source/Core/Kernel/KernelMemory.h
@@ -0,0 +1,33 @@
+/*
+ * KernelMemory.h
+ *
+ *  Created on: Apr 15, 2019
+ *      Author: rookyma
+ */
+
+#ifndef SOURCE_CORE_KERNEL_KERNELMEMORY_H_
+#define SOURCE_CORE_KERNEL_KERNELMEMORY_H_
+
+#include <cstring>
+
+namespace Kernel
+{
+
+template<typename T>
+static inline void ZeroMemory(T& value, size_t count = 1)
+{
+	size_t size = sizeof(T) * count;
+	std::memset(value, 0, size);
+}
+
+template<typename T>
+static inline size_t Memcpy(void* dest, const T& value, size_t count = 1)
+{
+	size_t size = sizeof(T) * count;
+	std::memcpy(dest, &value, size);
+	return size;
+}
+
+} /* namespace Kernel */
+
+#endif /* SOURCE_CORE_KERNEL_KERNELMEMORY_H_ */
