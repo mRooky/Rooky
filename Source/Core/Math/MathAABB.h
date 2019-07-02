d6a8cf6245147de237dd2aa976c47492ba7076ed
mRooky <rookyma@hotmail.com>
2019-07-02 23:23:45 +0800

    Math

--- Source/Core/Math/MathAABB.h
+++ Source/Core/Math/MathAABB.h
@@ -20,8 +20,26 @@ public:
 	~AABB(void) = default;
 
 public:
+	AABB(const Vector3f& min, const Vector3f& max) { Set(min, max); }
+
+public:
+	inline void Set(const Vector3f& min, const Vector3f& max)
+	{
+		this->min = min;
+		this->max = max;
+	}
+
+public:
 	inline Vector3f GetCenter(void) const { return (min + max) * 0.5f; }
 	inline float GetRadius(void) const { return (min - max).Length() * 0.5f; }
+
+public:
+	inline void SetMin(const Vector3f& min) { this->min = min; }
+	inline void SetMax(const Vector3f& max) { this->max = max; }
+
+public:
+	inline const Vector3f& GetMin(void) const { return min; }
+	inline const Vector3f& GetMax(void) const { return max; }
 
 public:
 	Vector3f min = {};
